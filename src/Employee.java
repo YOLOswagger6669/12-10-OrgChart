959be3719ddda4ab01962a844e045959a796516b
YOLOswagger6669 <samosley@gmail.com>
2014-12-11 21:04:00 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,68 @@
 // class to represent an emplayee - inherits Person
+public class Employee extends Person
+{
+  private String jobTitle;
+  private Double hourlyWage;
+  private int hoursWorked;
+  private Manager manager;
+  public Employee(double wage, int hours, String one, String a, int old, String job)
+  {
+    super(one, a, old);
+    hourlyWage=wage;
+    hoursWorked=hours;
+    jobTitle=job;
+  }
+  public String getName()
+  {
+    return super.toString();
+  }
+  public double getWage()
+  {
+    return hourlyWage;
+  }
+  public int getHours()
+  {
+    return hoursWorked;
+  }
+  public double getWeeklySalary()
+  {
+    return hoursWorked*hourlyWage;
+  }
+  public double getYearlySalary()
+  {
+    return 52*getWeeklySalary();
+  }
+  public String getJobTitle()
+  {
+    return jobTitle;
+  }
+  public String toString()
+  {
+    return super.toString()+": "+jobTitle+" @ "+getYearlySalary();
+  }
+  public Manager getManager()
+  {
+    return manager;
+  }
+  public void setWage(double newWage)
+  {
+    hourlyWage=newWage;
+  }
+  public void setHours(int h)
+  {
+    hoursWorked=h;
+  }
+  public void giveRaise()
+  {
+    hourlyWage++;
+  }
+  public void setManager(Manager newManager)
+  {
+    if(manager!=null)
+    {
+      manager.removeDirectReport(this);
+    }
+    manager=newManager;
+    manager.addDirectReport(this);
+  }
+}
